5979e63be22bab794c9d7552878f939e919ca3a3
Wolfgang Nagele <wnagele@datum.net>
2025-10-28 16:28:23 -0400

    Move to commons (#16)
    * Use IsHost from util
    
    * Move ParseSegments to util and adjust to []string since we don't use CLI anymore
    
    * Use DecodeSRv6Endpoint and EncodeSRv6Endpoint from util
    
    * Use Hex/Base64 convert from util
    
    * mod :broom:

--- srv6/srv6.go
+++ srv6/srv6.go
@@ -3,10 +3,7 @@ package srv6
 import (
 	"errors"
 	"fmt"
-	"net"
-	"strings"
 
-	"github.com/kenshaw/baseconv"
 	"github.com/vishvananda/netlink"
 
 	"github.com/datum-cloud/galactic-agent/srv6/neighborproxy"
@@ -20,15 +17,15 @@ func RouteIngressAdd(ipStr string) error {
 	if err != nil {
 		return fmt.Errorf("invalid ip: %w", err)
 	}
-	vpc, vpcAttachment, err := util.ExtractVPCFromSRv6Endpoint(ip)
+	vpc, vpcAttachment, err := util.DecodeSRv6Endpoint(ip)
 	if err != nil {
 		return fmt.Errorf("could not extract SRv6 endpoint: %w", err)
 	}
-	vpc, err = ToBase62(vpc)
+	vpc, err = util.HexToBase62(vpc)
 	if err != nil {
 		return fmt.Errorf("invalid vpc: %w", err)
 	}
-	vpcAttachment, err = ToBase62(vpcAttachment)
+	vpcAttachment, err = util.HexToBase62(vpcAttachment)
 	if err != nil {
 		return fmt.Errorf("invalid vpcattachment: %w", err)
 	}
@@ -44,15 +41,15 @@ func RouteIngressDel(ipStr string) error {
 	if err != nil {
 		return fmt.Errorf("invalid ip: %w", err)
 	}
-	vpc, vpcAttachment, err := util.ExtractVPCFromSRv6Endpoint(ip)
+	vpc, vpcAttachment, err := util.DecodeSRv6Endpoint(ip)
 	if err != nil {
 		return fmt.Errorf("could not extract SRv6 endpoint: %w", err)
 	}
-	vpc, err = ToBase62(vpc)
+	vpc, err = util.HexToBase62(vpc)
 	if err != nil {
 		return fmt.Errorf("invalid vpc: %w", err)
 	}
-	vpcAttachment, err = ToBase62(vpcAttachment)
+	vpcAttachment, err = util.HexToBase62(vpcAttachment)
 	if err != nil {
 		return fmt.Errorf("invalid vpcattachment: %w", err)
 	}
@@ -72,26 +69,26 @@ func RouteEgressAdd(prefixStr, srcStr string, segmentsStr []string) error {
 	if err != nil {
 		return fmt.Errorf("invalid src: %w", err)
 	}
-	segments, err := ParseSegments(segmentsStr)
+	segments, err := util.ParseSegments(segmentsStr)
 	if err != nil {
 		return fmt.Errorf("invalid segments: %w", err)
 	}
 
-	vpc, vpcAttachment, err := util.ExtractVPCFromSRv6Endpoint(src)
+	vpc, vpcAttachment, err := util.DecodeSRv6Endpoint(src)
 	if err != nil {
 		return fmt.Errorf("could not extract SRv6 endpoint: %w", err)
 	}
-	vpc, err = ToBase62(vpc)
+	vpc, err = util.HexToBase62(vpc)
 	if err != nil {
 		return fmt.Errorf("invalid vpc: %w", err)
 	}
-	vpcAttachment, err = ToBase62(vpcAttachment)
+	vpcAttachment, err = util.HexToBase62(vpcAttachment)
 	if err != nil {
 		return fmt.Errorf("invalid vpcattachment: %w", err)
 	}
 
 	var errs []error
-	if IsHost(prefix) {
+	if util.IsHost(prefix) {
 		if err := neighborproxy.Add(prefix, vpc, vpcAttachment); err != nil {
 			errs = append(errs, fmt.Errorf("neighborproxy add failed: %w", err))
 		}
@@ -114,26 +111,26 @@ func RouteEgressDel(prefixStr, srcStr string, segmentsStr []string) error {
 	if err != nil {
 		return fmt.Errorf("invalid src: %w", err)
 	}
-	segments, err := ParseSegments(segmentsStr)
+	segments, err := util.ParseSegments(segmentsStr)
 	if err != nil {
 		return fmt.Errorf("invalid segments: %w", err)
 	}
 
-	vpc, vpcAttachment, err := util.ExtractVPCFromSRv6Endpoint(src)
+	vpc, vpcAttachment, err := util.DecodeSRv6Endpoint(src)
 	if err != nil {
 		return fmt.Errorf("could not extract SRv6 endpoint: %w", err)
 	}
-	vpc, err = ToBase62(vpc)
+	vpc, err = util.HexToBase62(vpc)
 	if err != nil {
 		return fmt.Errorf("invalid vpc: %w", err)
 	}
-	vpcAttachment, err = ToBase62(vpcAttachment)
+	vpcAttachment, err = util.HexToBase62(vpcAttachment)
 	if err != nil {
 		return fmt.Errorf("invalid vpcattachment: %w", err)
 	}
 
 	var errs []error
-	if IsHost(prefix) {
+	if util.IsHost(prefix) {
 		if err := neighborproxy.Delete(prefix, vpc, vpcAttachment); err != nil {
 			errs = append(errs, fmt.Errorf("neighborproxy delete failed: %w", err))
 		}
@@ -145,32 +142,4 @@ func RouteEgressDel(prefixStr, srcStr string, segmentsStr []string) error {
 		return errors.Join(errs...)
 	}
 	return nil
-}
-
-func ToBase62(value string) (string, error) {
-	return baseconv.Convert(strings.ToLower(value), baseconv.DigitsHex, baseconv.Digits62)
-}
-
-func IsHost(ipNet *net.IPNet) bool {
-	ones, bits := ipNet.Mask.Size()
-	// host if mask is full length: /32 for IPv4, /128 for IPv6
-	return ones == bits
-}
-
-func ParseSegments(input []string) ([]net.IP, error) {
-	var segments []net.IP
-	for _, ipStr := range input {
-		ip, err := util.ParseIP(ipStr)
-		if err != nil {
-			return nil, fmt.Errorf("could not parse ip (%s): %v", ipStr, err)
-		}
-		if ip.To4() != nil {
-			return nil, fmt.Errorf("not an ipv6 address: %s", ipStr)
-		}
-		segments = append([]net.IP{ip}, segments...)
-	}
-	if len(segments) == 0 {
-		return nil, fmt.Errorf("no segments parsed: %v", input)
-	}
-	return segments, nil
 }
